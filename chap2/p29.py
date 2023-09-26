48a38442b45db03e3c161ad7a0d9d627f544e0bb
jungsungyun <tjddbs1201@naver.com>
2023-09-26 11:41:50 +0900

    task

---
+++ chap2/p29.py
@@ -0,0 +1,13 @@
+input1 = input("Hi.  What's your name? ")
+input2 = input("And how old are you? ")
+input3 = input("Okay, last question. How many pounds do you weigh? ")
+print("\n")
+print("Did you know that you're just 4 in dog years?\n")
+print("But you're also over 883008000 seconds old\n")
+print("If a small child were trying to get your attention, your name would become:")
+print(input1 *5)
+print("\n")
+print("Did you know that on the moon you would weigh only 27.5 pounds?")
+print("But on the sun, you'd weigh 4471.5 <but, ah... not for long>.\n\n")
+print("Press the enter key to exit.")
+
---
+++ chap2/p4.py
@@ -0,0 +1,2 @@
+print("Game Over\n")
+print("Press the enter key to exit.")
